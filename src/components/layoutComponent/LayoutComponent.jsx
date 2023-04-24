22040f0766c94d2b1c3eef6203f5345abf677fef
aiskairr <aminhansss2222@gmail.com>
2023-04-24 16:44:59 +0600

    fix/delete comment

--- src/components/layoutComponent/LayoutComponent.jsx
+++ src/components/layoutComponent/LayoutComponent.jsx
@@ -3,8 +3,8 @@ import scss from "./LayoutComponent.module.scss";
 const LayoutComponent = ({ children, title }) => {
   return (
     <section className={scss.wrapper}>
-      <h1 className={scss.wrapper_title}>{title}</h1> // текст заголовка 
-      <div className={scss.wrapper_item}>{children}</div> // основной контент
+      <h1 className={scss.wrapper_title}>{title}</h1>
+      <div className={scss.wrapper_item}>{children}</div>
     </section>
   );
 };
