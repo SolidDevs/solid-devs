45f24810d4adf760e85bd062d3839b79fdb37868
luchok22 <aitbaev2021@mail.ru>
2023-04-25 23:49:31 +0600

    fix: layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
+import Button from "@/components/Button/Button";
 import { useTranslation } from "react-i18next";
 const HomePage = () => {
   const { t } = useTranslation();
   return (
     <>
       <h1>{t("welcome")}</h1>
+      <Button title={'dasdssadasdasdsa dsaa'} withArrow={true}/>
     </>
   );
 };
