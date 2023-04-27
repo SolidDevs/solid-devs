8ff33d6f807c34c16f52180fd79fca524fba8a91
aiskairr <aminhansss2222@gmail.com>
2023-04-28 01:56:13 +0600

    fix/ deploy ci

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,5 @@
 import ItProjects from "@/components/It__projects/ItProjects";
 import MainContent from "@/components/MainContent/MainContent";
-import Navbar from "@/components/navbar/Navbar";
-import { navbarLinks } from "@/constants/navbar";
 import ServicesBlock from "@/components/servicesBlock/ServicesBlock";
 const HomePage = () => {
   return (
