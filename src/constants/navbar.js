29f6e885ac79f7fe4fa1948b6a98dbf46aed3014
aiskairr <aminhansss2222@gmail.com>
2023-04-27 22:56:38 +0600

    fix/ deploy ci

--- src/constants/navbar.js
+++ src/constants/navbar.js
@@ -1,20 +1,20 @@
 export const navbarLinks = [
-    {
-        title: "navbar.devOps"
-    },
-    {
-        title: "navbar.frontend"
-    },
-    {
-        title: "navbar.qualityControl"
-    },
-    {
-        title: "navbar.backend"
-    },
-    {
-        title: "navbar.mobilePhone"
-    },
-    {
-        title: "navbar.infrastructure"
-    }
+  {
+    title: "navbar.devOps"
+  },
+  {
+    title: "navbar.frontend"
+  },
+  {
+    title: "navbar.qualityControl"
+  },
+  {
+    title: "navbar.backend"
+  },
+  {
+    title: "navbar.mobilePhone"
+  },
+  {
+    title: "navbar.infrastructure"
+  }
 ]
