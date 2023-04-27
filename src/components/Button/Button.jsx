531ef62628f3d68e5efa8fee44e3d4d253881d6c
luchok22 <aitbaev2021@mail.ru>
2023-04-27 18:29:30 +0600

    fix: layout and variant

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,28 +1,34 @@
-import React from "react";
+import React, { useMemo } from "react";
 import scss from "./Button.module.scss";
 import arrowWhite from "../../../public/assets/ArrowWhite.svg";
 import arrowBlue from "../../../public/assets/ArrowBlue.svg";
 import Image from "next/image";
-const Button = ({ title, variable, withArrow }) => {
-  const backgorund__btn = variable
-    ? scss.button__btn_backgoundNon
-    : scss.button__btn;
-  const arrow = variable ? arrowBlue : arrowWhite;
-  return (
-    <div className={scss.button}>
-      <button className={backgorund__btn}>
+const Button = ({ title, variant, withArrow }) => {
+  let background__btn;
+  if (variant === "background_none") {
+    background__btn = scss.background__btn_non;
+  } else {
+    background__btn = scss.background__btn_active;
+  }
+
+  let arrow = variant ? arrowBlue : arrowWhite;
+  const render__btn = useMemo(
+    () => (
+      <button className={background__btn}>
         {title}
         {withArrow && (
           <Image
-            className={scss.button__btn__img}
+            className={scss.button__btn_img}
             src={arrow}
             width={20}
             alt="arrow"
           />
         )}
       </button>
-    </div>
+    ),
+    [title, withArrow]
   );
+  return <div className={scss.button}>{render__btn}</div>;
 };
 
 export default Button;
