8468d3f31c6e1a6e6b77a1c869862690daa00230
aiskairr <aminhansss2222@gmail.com>
2023-04-28 01:42:01 +0600

    fix/ add qp and add props data

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,33 @@
 import { useMemo } from "react";
 import scss from "./Navbar.module.scss";
-import { navbarLinks } from "@/constants/navbar";
 import { useTranslation } from "react-i18next";
+import { useRouter } from "next/router";
 
-const Navbar = () => {
+const Navbar = ({ navbarLinks }) => {
+  const router = useRouter();
+  let { tour } = router.query;
+  if (tour === undefined) {
+    tour = "1";
+  }
   const { t } = useTranslation("");
   const navbarItems = useMemo(
     () =>
       navbarLinks.map((item, index) => (
-        <a key={`${item.title}_${index}`} className={scss.navbar__item}>
+        <button
+          key={`${item.title}_${index}`}
+          onClick={() => {
+            const path = {
+              pathname: "",
+              search: `?tour=${item.title}`,
+            };
+            router.push(path, path, { shallow: true });
+          }}
+          className={scss.navbar__item}
+        >
           {t(`${item.title}`)}
-        </a>
+        </button>
       )),
-    []
+    [navbarLinks]
   );
   return <nav className={scss.navbar}>{navbarItems}</nav>;
 };
