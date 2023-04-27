2b4368a8394fbb599ce41bbe3540cfd1a9fadfe0
luchok22 <aitbaev2021@mail.ru>
2023-04-27 23:19:43 +0600

    fix: confilct

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,14 @@
 import Button from "@/components/Button/Button";
-import { useTranslation } from "react-i18next";
 import ItProjects from "@/components/It__projects/ItProjects";
 import MainContent from "@/components/MainContent/MainContent";
 import ServicesBlock from "@/components/servicesBlock/ServicesBlock";
 const HomePage = () => {
   return (
     <>
-      <h1>{t("weclome")}</h1>
       <Button title={"Связаться"}/>
       <Button title={"Перейти на сайт"} withArrow={true}/>
       <Button title={"Отправить резюме"} variant={"background_non"} withArrow={true}/>
       <Button title={"Обсудим ваш проект"} variant={"background_active"} withArrow={true}/>
-      <MainContent>
-        <ItProjects />
-        <ServicesBlock />
-      </MainContent>
     </>
   );
 };
