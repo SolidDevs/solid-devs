acd5858e689346de1e365b163adc42bc9b684472
aiskairr <aminhansss2222@gmail.com>
2023-04-28 01:48:32 +0600

    Revert "fix/ add qp and add props data"
    This reverts commit 8468d3f31c6e1a6e6b77a1c869862690daa00230.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,33 +1,18 @@
 import { useMemo } from "react";
 import scss from "./Navbar.module.scss";
+import { navbarLinks } from "@/constants/navbar";
 import { useTranslation } from "react-i18next";
-import { useRouter } from "next/router";
 
-const Navbar = ({ navbarLinks }) => {
-  const router = useRouter();
-  let { tour } = router.query;
-  if (tour === undefined) {
-    tour = "1";
-  }
+const Navbar = () => {
   const { t } = useTranslation("");
   const navbarItems = useMemo(
     () =>
       navbarLinks.map((item, index) => (
-        <button
-          key={`${item.title}_${index}`}
-          onClick={() => {
-            const path = {
-              pathname: "",
-              search: `?tour=${item.title}`,
-            };
-            router.push(path, path, { shallow: true });
-          }}
-          className={scss.navbar__item}
-        >
+        <a key={`${item.title}_${index}`} className={scss.navbar__item}>
           {t(`${item.title}`)}
-        </button>
+        </a>
       )),
-    [navbarLinks]
+    []
   );
   return <nav className={scss.navbar}>{navbarItems}</nav>;
 };
