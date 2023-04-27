5d5193b846c67bb69892f5c4e0bfb9aca07ace76
aiskairr <aminhansss2222@gmail.com>
2023-04-28 01:52:19 +0600

    fix/ add qp and add props data

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import ItProjects from "@/components/It__projects/ItProjects";
 import MainContent from "@/components/MainContent/MainContent";
 import Navbar from "@/components/navbar/Navbar";
+import { navbarLinks } from "@/constants/navbar";
 import { useTranslation } from "react-i18next";
 const HomePage = () => {
   const { t } = useTranslation();
@@ -9,7 +10,7 @@ const HomePage = () => {
       <h1>{t("welcome")}</h1>
       <MainContent>
         <ItProjects />
-        <Navbar />
+        <Navbar navbarLinks={navbarLinks} />
       </MainContent>
     </>
   );
