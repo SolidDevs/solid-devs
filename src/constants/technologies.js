ec53aa82817aed494fe6feb8012b25878f282052
aiskairr <aminhansss2222@gmail.com>
2023-04-27 21:41:07 +0600

    fix/deploy ci

--- src/constants/technologies.js
+++ src/constants/technologies.js
@@ -1,66 +1,66 @@
 export const technologiesInfo = [
-    {
-        title: "technologies.Terraform",
-        img: "./images/terraform.svg"
-    },
-    {
-        title: "technologies.Ansible",
-        img: "./images/ansible.svg"
-    },
-    {
-        title: "technologies.Jenkins",
-        img: "./images/jenkins.svg"
-    },
-    {
-        title: "technologies.Terraform",
-        img: "./images/terraform.svg"
-    },
-    {
-        title: "technologies.Ansible",
-        img: "./images/ansible.svg"
-    },
-    {
-        title: "technologies.Jenkins",
-        img: "./images/jenkins.svg"
-    },
-    {
-        title: "technologies.Ansible",
-        img: "./images/ansible.svg"
-    },
-    {
-        title: "technologies.Jenkins",
-        img: "./images/jenkins.svg"
-    },
-    {
-        title: "technologies.Terraform",
-        img: "./images/terraform.svg"
-    },
-    {
-        title: "technologies.Ansible",
-        img: "./images/ansible.svg"
-    },
-    {
-        title: "technologies.Jenkins",
-        img: "./images/jenkins.svg"
-    },
-    {
-        title: "technologies.Terraform",
-        img: "./images/terraform.svg"
-    },
-    {
-        title: "technologies.Ansible",
-        img: "./images/ansible.svg"
-    },
-    {
-        title: "technologies.Jenkins",
-        img: "./images/jenkins.svg"
-    },
-    {
-        title: "technologies.Ansible",
-        img: "./images/ansible.svg"
-    },
-    {
-        title: "technologies.Jenkins",
-        img: "./images/jenkins.svg"
-    },
-]
+  {
+    title: "technologies.Terraform",
+    img: "./images/terraform.svg",
+  },
+  {
+    title: "technologies.Ansible",
+    img: "./images/ansible.svg",
+  },
+  {
+    title: "technologies.Jenkins",
+    img: "./images/jenkins.svg",
+  },
+  {
+    title: "technologies.Terraform",
+    img: "./images/terraform.svg",
+  },
+  {
+    title: "technologies.Ansible",
+    img: "./images/ansible.svg",
+  },
+  {
+    title: "technologies.Jenkins",
+    img: "./images/jenkins.svg",
+  },
+  {
+    title: "technologies.Ansible",
+    img: "./images/ansible.svg",
+  },
+  {
+    title: "technologies.Jenkins",
+    img: "./images/jenkins.svg",
+  },
+  {
+    title: "technologies.Terraform",
+    img: "./images/terraform.svg",
+  },
+  {
+    title: "technologies.Ansible",
+    img: "./images/ansible.svg",
+  },
+  {
+    title: "technologies.Jenkins",
+    img: "./images/jenkins.svg",
+  },
+  {
+    title: "technologies.Terraform",
+    img: "./images/terraform.svg",
+  },
+  {
+    title: "technologies.Ansible",
+    img: "./images/ansible.svg",
+  },
+  {
+    title: "technologies.Jenkins",
+    img: "./images/jenkins.svg",
+  },
+  {
+    title: "technologies.Ansible",
+    img: "./images/ansible.svg",
+  },
+  {
+    title: "technologies.Jenkins",
+    img: "./images/jenkins.svg",
+  },
+];
