d7047714c386e8cc192088466b8ed7a69afc7769
aiskairr <aminhansss2222@gmail.com>
2023-04-28 20:16:13 +0600

    fix/change constants navbar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,6 @@
 import ItProjects from "@/components/It__projects/ItProjects";
 import MainContent from "@/components/MainContent/MainContent";
-import Navbar from "@/components/navbar/Navbar";
 import ServicesBlock from "@/components/servicesBlock/ServicesBlock";
-import { navbarLinks } from "@/constants/navbar";
 const HomePage = () => {
   return (
     <>
