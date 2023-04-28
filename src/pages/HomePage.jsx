01255fa996f073c28f5078f0df99d692077e0519
aiskairr <aminhansss2222@gmail.com>
2023-04-28 21:25:08 +0600

    feat/ create technologies in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,14 @@ import ItProjects from "@/components/It__projects/ItProjects";
 import MainContent from "@/components/MainContent/MainContent";
 import OurTeam from "@/components/our__team/OurTeam";
 import ServicesBlock from "@/components/servicesBlock/ServicesBlock";
+import TechnologiesBlock from "@/components/technologiesBlock/TechnologiesBlock";
 const HomePage = () => {
   return (
     <>
       <MainContent>
         <ItProjects />
         <ServicesBlock />
+        <TechnologiesBlock />
         <OurTeam />
       </MainContent>
     </>
