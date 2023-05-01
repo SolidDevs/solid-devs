9c9ae7a0b23339097312362b0fce4aa5f63dce86
luchok22 <aitbaev2021@mail.ru>
2023-05-01 17:20:45 +0600

    fix: rename

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,10 @@
 import Footer from "@/components/Footer/Footer";
 import ItProjects from "@/components/It__projects/ItProjects";
 import MainContent from "@/components/MainContent/MainContent";
-import FeedBack from "@/components/feedBack/FeedBack";
 import OurTeam from "@/components/our__team/OurTeam";
 import ServicesBlock from "@/components/servicesBlock/ServicesBlock";
 import TechnologiesBlock from "@/components/technologiesBlock/TechnologiesBlock";
+import FeedBack from "@/components/feedBack/FeedBack";
 const HomePage = () => {
   return (
     <>
