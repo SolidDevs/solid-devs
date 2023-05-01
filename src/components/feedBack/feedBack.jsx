dcb62899ad61df01f5fdef13716f2fdd6b03d270
luchok22 <aitbaev2021@mail.ru>
2023-05-01 16:47:39 +0600

    fix: name components

--- src/components/feedBack/feedBack.jsx
+++ src/components/feedBack/feedBack.jsx
@@ -1,7 +1,7 @@
 import React, { useMemo } from "react";
 import scss from "./FeedBack.module.scss";
 import { feedBackArr } from "@/constants/feedBack";
-import FeedBackCard from "./FeedBackCard/FeedBackCard.jsx";
+import FeedBackCard from "./FeedBackCard/FeedBackCard";
 const FeedBack = () => {
   const renderCard = useMemo(
     () => feedBackArr.map((el) => <FeedBackCard {...el} key={el.title} />),
