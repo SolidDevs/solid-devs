4b05cca46614b2a945e1479745f6f658a9c4bebc
luchok22 <aitbaev2021@mail.ru>
2023-05-01 12:43:55 +0600

    fix: spaces

--- src/components/feed__back/feedBack.jsx
+++ src/components/feed__back/feedBack.jsx
@@ -1,17 +1,15 @@
 import React, { useMemo } from "react";
 import scss from "./feedBack.module.scss";
 import FeedBackCard from "./feed__back_card/feedBackCard";
-import { useTranslation } from "react-i18next";
 import { feedBackArr } from "@/constants/feedBack";
 const FeedBack = () => {
-  const { t } = useTranslation();
   const renderCard = useMemo(
     () => feedBackArr.map((el) => <FeedBackCard {...el} key={el.title} />),
     [feedBackArr]
   );
   return (
     <div className={scss.talk}>
-      <h1>{t("feedBack.call")}</h1>
+      <h1>What people say</h1>
       <div className={scss.talk__slide}>{renderCard}</div>
     </div>
   );
