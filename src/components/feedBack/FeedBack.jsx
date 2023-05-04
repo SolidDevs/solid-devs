6e1945cbf155cfb4e2c3e0aef92cba25e36070c1
luchok22 <aitbaev2021@mail.ru>
2023-05-04 23:22:50 +0600

    fix: rename

--- src/components/feedBack/FeedBack.jsx
+++ src/components/feedBack/FeedBack.jsx
@@ -1,11 +1,11 @@
 import React, { useMemo } from "react";
 import scss from "./FeedBack.module.scss";
 import { feedBackArr } from "@/constants/feedBack";
-import FeedBackCard from "./FeedBackCard/FeedBackCard";
 import { useTranslation } from "react-i18next";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+import FeedBackCard from "./feedBackCard/FeedBackCard";
 const FeedBack = ({sliderSettings}) => {
   const { t } = useTranslation();
   const renderCard = useMemo(
