bef1d480eac873b35bd31eda304967da4f294493
luchok22 <aitbaev2021@mail.ru>
2023-05-04 23:32:41 +0600

    fix: spaces

--- src/components/feedBack/FeedBack.jsx
+++ src/components/feedBack/FeedBack.jsx
@@ -3,10 +3,10 @@ import scss from "./FeedBack.module.scss";
 import { feedBackArr } from "@/constants/feedBack";
 import { useTranslation } from "react-i18next";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import FeedBackCard from "./feedBackCard/FeedBackCard";
-const FeedBack = ({sliderSettings}) => {
+const FeedBack = ({ sliderSettings }) => {
   const { t } = useTranslation();
   const renderCard = useMemo(
     () => feedBackArr.map((el) => <FeedBackCard {...el} key={el.title} />),
@@ -16,9 +16,7 @@ const FeedBack = ({sliderSettings}) => {
     <div className={scss.talk}>
       <h1>{t("feedBack.call")}</h1>
       <div className={scss.talk__slide}>
-      <Slider {...sliderSettings}>
-        {renderCard}
-      </Slider>
+        <Slider {...sliderSettings}>{renderCard}</Slider>
       </div>
     </div>
   );
