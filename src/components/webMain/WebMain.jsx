46c621a973a64d3ffa960c573ede88fcab33631b
Improove3567 <jyldyzbekov06aiti@gmail.com>
2023-05-05 19:40:31 +0600

    fix:issues

--- src/components/webMain/WebMain.jsx
+++ src/components/webMain/WebMain.jsx
@@ -2,10 +2,17 @@ import React, { useMemo } from "react";
 import scss from "./WebMain.module.scss";
 import { useTranslation } from "react-i18next";
 import Button from "../Button/Button";
+import Image from "next/image";
 
 const WebMain = () => {
   const { t } = useTranslation();
-  const renderText = useMemo(
+
+  const imageStyle = {
+    borderRadius: '50%',
+    border: '1px solid #666',
+  };
+
+  const renderContent = useMemo(
     () => (
       <div className={scss.content__text__wrapper}>
         <div className={scss.content__title} >
@@ -23,8 +30,14 @@ const WebMain = () => {
     <div className={scss.main}>
       <div className="container">
         <div className={scss.main__content}>
-          {renderText}
-          <img src='./images/webMain/webMainImage.png' alt="website image" className={scss.content__image} />
+          {renderContent}
+          <img
+            src='/images/webMain/webMainImage.png'
+            alt="website image"
+            className={scss.content__image}
+            width={500}
+            height={500}
+          />
         </div>
       </div>
     </div>
