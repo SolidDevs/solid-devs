bb144e4f2cdb4f2902144ab3396d689a332bbc4a
Improove3567 <jyldyzbekov06aiti@gmail.com>
2023-05-06 05:24:13 +0600

    feat/servicesAndProcess

--- src/pages/WebServicePage.jsx
+++ src/pages/WebServicePage.jsx
@@ -1,9 +1,12 @@
 import Footer from "@/components/Footer/Footer";
+import ServicesAndProcess from "@/components/servicesAndProcess/ServicesAndProcess";
 import WebMain from "@/components/webMain/WebMain";
 const WebServicePage = () => {
   return (
     <>
       <WebMain />
+      <ServicesAndProcess />
+      <ServicesAndProcess variant="process" />
       <Footer />
     </>
   );
