b466feaa807a5c363b1f09814ff166c4709c9f74
temangi <temirlan2.nuri@gmail.com>
2023-05-06 13:33:34 +0600

    fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useMemo, useState } from "react";
 import Image from "next/image";
 import arrow from "/public/images/Header/arrow.svg";
 import scss from "./Header.module.scss";
@@ -9,37 +9,47 @@ import i18n from "i18next";
 
 const Header = () => {
   const { t } = i18n;
-  const [index, setIndex] = useState(false);
-  const [lan, setLan] = useState(false);
-  const [en, setEn] = useState(t("header.languageFullEn"));
-  const [ru, setRu] = useState(t("header.languageFullRu"));
-  const [language, setLanguage] = useState(t("header.languageEn"));
+  const [isLanguageOpen, setIsLanguageOpen] = useState(false);
+  const [isRussianLanguage, setIsRussianLanguage] = useState(false);
   const { route } = useRouter();
-  const click = () => setIndex(!index);
+  const click = () => setIsLanguageOpen(!isLanguageOpen);
   const change = () => {
-    setLan(!lan);
-    setIndex(!index);
-    i18n.changeLanguage(!lan ? "ru" : "en");
+    setIsRussianLanguage(!isRussianLanguage);
+    i18n.changeLanguage(isRussianLanguage ? "en" : "ru");
   };
-  useEffect(() => {
-    setLanguage(!lan ? t("header.languageEn") : t("header.languageRu"));
-    setRu(lan ? t("header.languageFullEn") : t("header.languageFullRu"));
-    setEn(!lan ? t("header.languageFullEn") : t("header.languageFullRu"));
-  }, [lan]);
+
+  const language = useMemo(
+    () => (isRussianLanguage ? t("header.languageRu") : t("header.languageEn")),
+    [isRussianLanguage]
+  );
+  const languageFull = useMemo(
+    () =>
+      isRussianLanguage
+        ? t("header.languageFullRu")
+        : t("header.languageFullEn"),
+    [isRussianLanguage]
+  );
+  const languageSecondFull = useMemo(
+    () =>
+      !isRussianLanguage
+        ? t("header.languageFullRu")
+        : t("header.languageFullEn"),
+    [isRussianLanguage]
+  );
 
   return (
     <section className={scss.header}>
-      <aside className={scss.header_left}>
+      <aside className={scss.header__left}>
         <Link href={"/"}>
-          <span className={scss.header_left__title}>SOLID DEVS</span>
+          <span className={scss.header__leftTitle}>SOLID DEVS</span>
         </Link>
-        <label className={scss.header_left__items}>
+        <nav className={scss.header__leftNavs}>
           <Link
             href={"/about"}
             className={
               route == "/about"
-                ? scss.header_left__items__isActivelink
-                : scss.header_left__items__link
+                ? scss.header__leftItemsIsActiveLink
+                : scss.header__leftItemsLink
             }
           >
             <p>{t("header.about")}</p>
@@ -48,34 +58,40 @@ const Header = () => {
             href={"/career"}
             className={
               route == "/about"
-                ? scss.header_left__items__isActivelink
-                : scss.header_left__items__link
+                ? scss.header__leftItemsIsActiveLink
+                : scss.header__leftItemsLink
             }
           >
             <p>{t("header.career")}</p>
           </Link>
-          <div className={scss.header_left__items__option}>
+          <div className={scss.header__leftItemsOption}>
             <p>{t("header.service")}</p>
             <Image
               src={arrow}
               alt="arrow"
               width={16}
               height={8}
-              style={{ margin: "0 10px" }}
             />
           </div>
-        </label>
+        </nav>
       </aside>
-      <aside className={scss.header_right}>
-        <div className={scss.header_right__language} onClick={click}>
-          <p>{language}</p>
+      <aside className={scss.header__right}>
+        <div className={scss.header__rightLanguage} onClick={click}>
+          <p
+             style={{ marginRight: isRussianLanguage ? "40px" : "45px" }}
+          >
+            {language}
+          </p>
           <Image src={arrow} alt="arrow" width={16} height={8} />
         </div>
         <Button title={t("header.button")} withArrow={false} />
-        {index && (
-          <label className={scss.header_right__option}>
-            <p onClick={change}>{ru}</p>
-            <p>{en}</p>
+        {isLanguageOpen && (
+          <label
+            className={scss.header__rightOption}
+            style={{ right: isRussianLanguage ? "200px" : "177px" }}
+          >
+            <p onClick={change}>{languageSecondFull}</p>
+            <p>{languageFull}</p>
           </label>
         )}
       </aside>
