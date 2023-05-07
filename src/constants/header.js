9b7557209d16e6e1091b18c0a0dbd8931c1ac236
temangi <temirlan2.nuri@gmail.com>
2023-05-07 15:13:08 +0600

    fix

--- src/constants/header.js
+++ src/constants/header.js
@@ -1,14 +1,14 @@
 export const navs = [
-    {
-        link : "/aboutUs",
-        text : "header.about"
-    },
-    {
-        link : "/career",
-        text : "header.career"
-    },
-    {
-        link : "/webService",
-        text : "header.service"
-    }
-]
+  {
+    link: "/aboutUs",
+    text: "header.about",
+  },
+  {
+    link: "/career",
+    text: "header.career",
+  },
+  {
+    link: "/webService",
+    text: "header.service",
+  },
+];
