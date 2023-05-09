8f2e43c1c98db64a60c5145618ad7b01f74cea55
Almo <temirkanovalmanbet5@gmail.com>
2023-05-09 20:20:45 +0300

    fix:reusable component

--- src/pages/WebServicePage.jsx
+++ src/pages/WebServicePage.jsx
@@ -1,10 +1,12 @@
 import Footer from "@/components/Footer/Footer";
+import Header from "@/components/Header/Header";
 import BenefitsAndStatistics from "@/components/benefitsAndStatistics/BenefitsAndStatistics";
 import ServicesAndProcess from "@/components/servicesAndProcess/ServicesAndProcess";
 import WebMain from "@/components/webMain/WebMain";
 const WebServicePage = () => {
   return (
     <>
+      <Header />
       <WebMain />
       <ServicesAndProcess />
       <ServicesAndProcess variant="process" />
