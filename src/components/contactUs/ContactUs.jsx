eb61eecde25f39cc224293fce8a2b752c3b1dca5
luchok22 <aitbaev2021@mail.ru>
2023-05-09 19:08:49 +0600

    feat: contact us in about us page

---
+++ src/components/contactUs/ContactUs.jsx
@@ -0,0 +1,56 @@
+import React, { useMemo } from "react";
+import scss from "./ContactsUs.module.scss";
+import Button from "../Button/Button";
+import { contactUsCardArr } from "@/constants/contactUs";
+import Image from "next/image";
+import { useTranslation } from "react-i18next";
+const ContactUs = () => {
+  const { t } = useTranslation();
+  const renderCards = useMemo(
+    () =>
+      contactUsCardArr.map((el, index) => (
+        <div className={scss.contactUs__card} id={index}>
+          <div className={scss.card__img}>
+            <Image src={el.avatar} width={59} height={59} alt={el.title} />
+          </div>
+          <div className={scss.card__title}>
+            <p>{t(el.title)}</p>
+          </div>
+        </div>
+      )),
+    []
+  );
+  return (
+    <div className={scss.contactUs}>
+      <div className="container">
+        <div className={scss.contactUs__wrapper}>
+          <div className={scss.contactUs__info}>
+            <div className={scss.info__title}>
+              <p>{t("contactUs.title")}</p>
+            </div>
+            <div className={scss.info__subtitle}>
+              <p>{t("contactUs.subtitle")}</p>
+            </div>
+            <div className={scss.info__btns}>
+              <div className={scss.btns__project}>
+                <Button
+                  title={"button__reuseble.project"}
+                  variant={"btn__light_blue"}
+                />
+              </div>
+              <div className={scss.btns__mail}>
+                <Button
+                  title={"button__reuseble.mail"}
+                  variant={"btn__no_bg"}
+                />
+              </div>
+            </div>
+          </div>
+          <div className={scss.contactUs__cards}>{renderCards}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactUs;
---
+++ src/constants/contactUs.js
@@ -0,0 +1,11 @@
+import avatar from "../../public/assets/images/contactUs/avatar.svg";
+export const contactUsCardArr = [
+  {
+    avatar: avatar,
+    title: "contactUs.card__title",
+  },
+  {
+    avatar: avatar,
+    title: "contactUs.card__title",
+  },
+];
