d1bc32da1b6cc136484f2ab9bcbb5d41dc2066bd
Almo <temirkanovalmanbet5@gmail.com>
2023-05-12 17:50:17 +0300

    fix: columns-Reusable-component

--- src/components/columnsReusableComponent/reasonsCard/ReasonsCard.jsx
+++ src/components/columnsReusableComponent/reasonsCard/ReasonsCard.jsx
@@ -1,11 +1,16 @@
 import { useTranslation } from "react-i18next";
 import scss from "./ReasonsCard.module.scss";
-import React from "react";
+import React, { useMemo } from "react";
+import Image from "next/image";
+import icon from "../../../../public/images/webServices/serviceIcon.png"
 
-const ReasonsCard = ({ title, description }) => {
+const ReasonsCard = ({ title, description}) => {
   const { t } = useTranslation();
   return (
     <div className={scss.reasons__card}>
+      <div className={scss.reasons__card__icon}>
+        <Image src={icon} alt="icon" width={27} height={16} />
+      </div>
       <h3>{t(title)}</h3>
       <p>{t(description)}</p>
     </div>
