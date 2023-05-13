8b78d4f0d2abfc18d05fffcea2eae6c5cf519c97
Improove3567 <jyldyzbekov06aiti@gmail.com>
2023-05-13 21:27:10 +0600

    feat:custom hooks

---
+++ src/hooks/useService.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import {
+  collection,
+  getDocs,
+  query,
+} from "firebase/firestore";
+import { db } from '@/firebase/firebase';
+
+const useService = (collection) => {
+  const [services, setServices] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  const getServices = async () => {
+    const servicesArr = []
+    const getFireStore = query(collection(db, collection))
+    const querySnapshot = await getDocs(getFireStore);
+    querySnapshot.forEach((doc) => servicesArr.push({ tid: doc.id, ...doc.data() }));
+    setServices(servicesArr);
+    setLoading(false)
+  }
+
+  return {
+    services,
+    getServices,
+    isLoading,
+  };
+};
+
+export default useService;
