ed6d378e87034658c332d2b1b3d458e39a6d3fcf
luchok22 <aitbaev2021@mail.ru>
2023-05-13 17:15:53 +0600

    feat: add components for all pages

---
+++ src/constants/reusebleService.js
@@ -0,0 +1,331 @@
+export const uiWeb = [
+  {
+    desc: "uiWeb.title",
+    title: "uiWeb.card__title",
+    icon: "/images/webServices/serviceIcon.png",
+    description: "uiWeb.card__subtitle",
+  },
+  {
+    icon: "/images/webServices/serviceIcon.png",
+    desc: "uiWeb.title",
+    title: "uiWeb.card__title",
+    description: "uiWeb.card__subtitle",
+  },
+];
+export const uiProcess = [
+  {
+    desc: "uiProcess.title",
+    title: "uiProcess.card__title",
+    description: "uiProcess.card__subtitle",
+  },
+  {
+    desc: "uiProcess.title",
+    title: "uiProcess.card__title",
+    description: "uiProcess.card__subtitle",
+  },
+  {
+    desc: "uiProcess.title",
+    title: "uiProcess.card__title",
+    description: "uiProcess.card__subtitle",
+  },
+  {
+    desc: "uiProcess.title",
+    title: "uiProcess.card__title",
+    description: "uiProcess.card__subtitle",
+  },
+  {
+    desc: "uiProcess.title",
+    title: "uiProcess.card__title",
+    description: "uiProcess.card__subtitle",
+  },
+  {
+    desc: "uiProcess.title",
+    title: "uiProcess.card__title",
+    description: "uiProcess.card__subtitle",
+  },
+];
+
+export const mobileWeb = [
+  {
+    icon: "/images/webServices/serviceIcon.png",
+    desc: "mobileWeb.title",
+    title: "mobileWeb.card__title",
+    description: "mobileWeb.card__subtitle",
+  },
+  {
+    desc: "mobileWeb.title",
+    title: "mobileWeb.card__title",
+    icon: "/images/webServices/serviceIcon.png",
+    description: "mobileWeb.card__subtitle",
+  },
+  {
+    desc: "mobileWeb.title",
+    icon: "/images/webServices/serviceIcon.png",
+    title: "mobileWeb.card__title",
+    description: "mobileWeb.card__subtitle",
+  },
+  {
+    desc: "mobileWeb.title",
+    title: "mobileWeb.card__title",
+    icon: "/images/webServices/serviceIcon.png",
+    description: "mobileWeb.card__subtitle",
+  },
+  {
+    desc: "mobileWeb.title",
+    title: "mobileWeb.card__title",
+    icon: "/images/webServices/serviceIcon.png",
+    description: "mobileWeb.card__subtitle",
+  },
+];
+
+export const mobileProcess = [
+  {
+    desc: "mobileProcess.title",
+    title: "mobileProcess.card__title",
+    description: "mobileProcess.card__subtitle",
+  },
+  {
+    desc: "mobileProcess.title",
+    title: "mobileProcess.card__title",
+    description: "mobileProcess.card__subtitle",
+  },
+  {
+    desc: "mobileProcess.title",
+    title: "mobileProcess.card__title",
+    description: "mobileProcess.card__subtitle",
+  },
+  {
+    desc: "mobileProcess.title",
+    title: "mobileProcess.card__title",
+    description: "mobileProcess.card__subtitle",
+  },
+  {
+    desc: "mobileProcess.title",
+    title: "mobileProcess.card__title",
+    description: "mobileProcess.card__subtitle",
+  },
+];
+export const mvpWeb = [
+  {
+    desc: "mvpWeb.title",
+    title: "mvpWeb.card__title",
+    description: "mvpWeb.card__subtitle",
+    icon: "/images/webServices/serviceIcon.png",
+  },
+  {
+    desc: "mvpWeb.title",
+    title: "mvpWeb.card__title",
+    icon: "/images/webServices/serviceIcon.png",
+    description: "mvpWeb.card__subtitle",
+  },
+  {
+    icon: "/images/webServices/serviceIcon.png",
+    desc: "mvpWeb.title",
+    icon: "/images/webServices/serviceIcon.png",
+    title: "mvpWeb.card__title",
+    description: "mvpWeb.card__subtitle",
+  },
+  {
+    icon: "/images/webServices/serviceIcon.png",
+    desc: "mvpWeb.title",
+    title: "mvpWeb.card__title",
+    description: "mvpWeb.card__subtitle",
+  },
+  {
+    desc: "mvpWeb.title",
+    title: "mvpWeb.card__title",
+    icon: "/images/webServices/serviceIcon.png",
+    description: "mvpWeb.card__subtitle",
+  },
+  {
+    icon: "/images/webServices/serviceIcon.png",
+    desc: "mvpWeb.title",
+    title: "mvpWeb.card__title",
+    description: "mvpWeb.card__subtitle",
+  },
+];
+
+export const mvpProcess = [
+  {
+    desc: "mvpProcess.title",
+    title: "mvpProcess.card__title",
+    description: "mvpProcess.card__subtitle",
+  },
+  {
+    desc: "mvpProcess.title",
+    title: "mvpProcess.card__title",
+    description: "mvpProcess.card__subtitle",
+  },
+  {
+    desc: "mvpProcess.title",
+    title: "mvpProcess.card__title",
+    description: "mvpProcess.card__subtitle",
+  },
+  {
+    desc: "mvpProcess.title",
+    title: "mvpProcess.card__title",
+    description: "mvpProcess.card__subtitle",
+  },
+  {
+    desc: "mvpProcess.title",
+    title: "mvpProcess.card__title",
+    description: "mvpProcess.card__subtitle",
+  },
+  {
+    desc: "mvpProcess.title",
+    title: "mvpProcess.card__title",
+    description: "mvpProcess.card__subtitle",
+  },
+];
+
+export const mvpBlock = [
+  {
+    desc: "mvp.title",
+    title: "mvp.card__title",
+    description: "mvp.card__subtitle",
+    icon: "/images/webServices/serviceIcon.png",
+  },
+  {
+    desc: "mvp.title",
+    title: "mvp.card__title",
+    icon: "/images/webServices/serviceIcon.png",
+    description: "mvp.card__subtitle",
+  },
+  {
+    desc: "mvp.title",
+    title: "mvp.card__title",
+    icon: "/images/webServices/serviceIcon.png",
+    description: "mvp.card__subtitle",
+  },
+];
+
+export const chatWeb = [
+  {
+    desc: "chatWeb.title",
+    title: "chatWeb.card__title",
+    icon: "/images/webServices/serviceIcon.png",
+    description: "chatWeb.card__subtitle",
+  },
+  {
+    desc: "chatWeb.title",
+    title: "chatWeb.card__title",
+    icon: "/images/webServices/serviceIcon.png",
+    description: "chatWeb.card__subtitle",
+  },
+  {
+    desc: "chatWeb.title",
+    icon: "/images/webServices/serviceIcon.png",
+    title: "chatWeb.card__title",
+    description: "chatWeb.card__subtitle",
+  },
+];
+export const chatPriority = [
+  {
+    desc: "chatPriority.title",
+    title: "chatPriority.connection",
+    icon: "/images/webServices/check-mark.png",
+  },
+  {
+    desc: "chatInfo.title",
+    title: "chatPriority.performance",
+    icon: "/images/webServices/check-mark.png",
+  },
+  {
+    desc: "chatInfo.title",
+    title: "chatPriority.storage",
+    icon: "/images/webServices/check-mark.png",
+  },
+  {
+    desc: "chatInfo.title",
+    title: "chatPriority.assistance",
+    icon: "/images/webServices/check-mark.png",
+  },
+  {
+    desc: "chatInfo.title",
+    title: "chatPriority.barrier",
+    icon: "/images/webServices/check-mark.png",
+  },
+  {
+    desc: "chatInfo.title",
+    title: "chatPriority.conversions",
+    icon: "/images/webServices/check-mark.png",
+  },
+];
+
+export const chatInformation = [
+  {
+    desc: "chatInfo.title",
+    title: "chatInfo.experience",
+    accounting: "3",
+  },
+  {
+    desc: "chatInfo.title",
+    title: "chatInfo.successes",
+    accounting: "4",
+  },
+];
+
+export const webServices = [
+  {
+    desc: "webServices.title",
+    title: "webServices.card__title",
+    description: "webServices.card__subtitle",
+  },
+  {
+    desc: "webServices.title",
+    title: "webServices.card__title",
+    description: "webServices.card__subtitle",
+  },
+  {
+    desc: "webServices.title",
+    title: "webServices.card__title",
+    description: "webServices.card__subtitle",
+  },
+  {
+    desc: "webServices.title",
+    title: "webServices.card__title",
+    description: "webServices.card__subtitle",
+  },
+  {
+    desc: "webServices.title",
+    title: "webServices.card__title",
+    description: "webServices.card__subtitle",
+  },
+  {
+    desc: "webServices.title",
+    title: "webServices.card__title",
+    description: "webServices.card__subtitle",
+  },
+];
+export const webProcess = [
+  {
+    desc: "webProcess.title",
+    title: "webProcess.card__title",
+    description: "webProcess.card__subtitle",
+  },
+  {
+    desc: "webProcess.title",
+    title: "webProcess.card__title",
+    description: "webProcess.card__subtitle",
+  },
+  {
+    desc: "webProcess.title",
+    title: "webProcess.card__title",
+    description: "webProcess.card__subtitle",
+  },
+  {
+    desc: "webProcess.title",
+    title: "webProcess.card__title",
+    description: "webProcess.card__subtitle",
+  },
+  {
+    desc: "webProcess.title",
+    title: "webProcess.card__title",
+    description: "webProcess.card__subtitle",
+  },
+  {
+    desc: "webProcess.title",
+    title: "webProcess.card__title",
+    description: "webProcess.card__subtitle",
+  },
+];
--- src/pages/MobileDevPage.jsx
+++ src/pages/MobileDevPage.jsx
@@ -1,13 +1,19 @@
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
+import ContactUs from "@/components/contactUs/ContactUs";
+import ServicesAndProcess from "@/components/servicesAndProcess/ServicesAndProcess";
 import ReusableMainContent from "@/components/webMain/WebMain";
 import { mobileMain } from "@/constants/reusableMain";
+import { mobileProcess, mobileWeb } from "@/constants/reusebleService";
 
 const MobileDevPage = () => {
   return (
     <>
       <Header />
       <ReusableMainContent data={mobileMain} />
+      <ServicesAndProcess data={mobileWeb}/>
+      <ServicesAndProcess variant={"process"} data={mobileProcess} />
+      <ContactUs />
       <Footer />
     </>
   );
