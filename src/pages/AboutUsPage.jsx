a4b6cd76d48c0ab1417333d75b6a33d5ef88dc92
Almo <temirkanovalmanbet5@gmail.com>
2023-05-13 21:45:15 +0300

    feat/component-Preloader22

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -9,12 +9,12 @@ import FeedBack from "@/components/feedBack/FeedBack";
 const AboutUsPage = () => {
   return (
     <>
-      <Header/>
+      <Header />
       <AboutUs />
       <Preloader />
       <ReasonsBlock />
       <FeedBack />
-      <ContactUs/>
+      <ContactUs />
       <Footer />
     </>
   );
