cea5182b3d673a041563a6a33747f2b6641d11f7
luchok22 <aitbaev2021@mail.ru>
2023-05-13 22:36:05 +0600

    fix: spaces in hooks

--- src/hooks/useProject.js
+++ src/hooks/useProject.js
@@ -1,23 +1,21 @@
-import { useState } from 'react';
-import {
-  collection,
-  getDocs,
-  query,
-} from "firebase/firestore";
-import { db } from '@/firebase/firebase';
+import { useState } from "react";
+import { collection, getDocs, query } from "firebase/firestore";
+import { db } from "@/firebase/firebase";
 
 const useProject = () => {
   const [projects, setProjects] = useState([]);
   const [isLoading, setLoading] = useState(true);
 
   const getProjects = async () => {
-    const projectsArr = []
-    const getFireStore = query(collection(db, "projects"))
+    const projectsArr = [];
+    const getFireStore = query(collection(db, "projects"));
     const querySnapshot = await getDocs(getFireStore);
-    querySnapshot.forEach((doc) => projectsArr.push({ tid: doc.id, ...doc.data() }));
+    querySnapshot.forEach((doc) =>
+      projectsArr.push({ tid: doc.id, ...doc.data() })
+    );
     setProjects(projectsArr);
-    setLoading(false)
-  }
+    setLoading(false);
+  };
 
   return {
     projects,
