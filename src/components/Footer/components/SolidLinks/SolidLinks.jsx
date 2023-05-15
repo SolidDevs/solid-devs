a9e18b8fb82cd6125e9cd1aef0d1c0dfe252cbd7
Serenbek Ibraimkulov <serenbek@mail.ru>
2023-05-15 19:54:12 +0600

    addedTranslation

--- src/components/Footer/components/SolidLinks/SolidLinks.jsx
+++ src/components/Footer/components/SolidLinks/SolidLinks.jsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import scss from "./SolidLinks.module.scss";
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 const SolidLinks = () => {
+  const { t } = useTranslation();
   return (
     <div className={scss.links__main}>
-      <Link href="/aboutUs">О нас</Link>
-      <Link href="/carier">Карьера</Link>
+      <Link href="/aboutUs">{t("solidLinks.about")}</Link>
+      <Link href="/carier">{t("solidLinks.career")}</Link>
     </div>
   );
 };
