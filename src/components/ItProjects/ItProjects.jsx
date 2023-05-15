cbf5234332a3c15e90bd42912c1951777d8f7745
luchok22 <aitbaev2021@mail.ru>
2023-05-15 17:25:09 +0600

    feat: adaptive it projects in homepage

--- src/components/ItProjects/ItProjects.jsx
+++ src/components/ItProjects/ItProjects.jsx
@@ -1,12 +1,14 @@
 import scss from "./ItProjects.module.scss";
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import ItProjectsCard from "./ItProjectsCard/ItProjectsCard";
 import { itProjects } from "@/constants/itProjects";
 import SectionContainer from "../layoutComponent/SectionContainer";
 import { useTranslation } from "react-i18next";
-
+import Image from "next/image";
+import vector from "../../../public/assets/images/itProjects/vector.svg";
 const ItProjects = () => {
   const { t } = useTranslation();
+  const [isModal, setModal] = useState(false);
   const itProjectsRender = useMemo(
     () =>
       itProjects.map((item, index) => (
@@ -15,9 +17,23 @@ const ItProjects = () => {
     []
   );
   return (
-    <SectionContainer title={t("sectionContainer.project__title")}>
-      <div className={scss.cards__main}>{itProjectsRender}</div>
-    </SectionContainer>
+    <div className={scss.projects}>
+      <div className={scss.projects__cards}>
+        <SectionContainer title={t("sectionContainer.project__title")}>
+          <div className={scss.cards__main}>{itProjectsRender}</div>
+        </SectionContainer>
+      </div>
+      <div className={scss.projects__adaptive}>
+        <div className={scss.title} onClick={() => setModal(!isModal)}>
+          <p>{t("sectionContainer.project__title")}</p>
+          <Image src={vector} width={14} height={8} alt="vector" />
+        </div>
+
+          {isModal && (
+            <div className={scss.cards__main}>{itProjectsRender}</div>
+          )}
+      </div>
+    </div>
   );
 };
 
