ab8543ae58eceb4a3ee49af14489edab8c3bdb6a
temangi <temirlan2.nuri@gmail.com>
2023-05-18 17:49:45 +0600

    fix

--- src/components/ourProjects/OurProjects.jsx
+++ src/components/ourProjects/OurProjects.jsx
@@ -6,14 +6,9 @@ import SampleNextArrow from "./arrows/nextArrow/SampleNextArrow";
 import SamplePrevArrow from "./arrows/prevArrow/SamplePrevArrow";
 import Paging from "./paging/Paging";
 import ProjectsItem from "./projectsItem/ProjectsItem";
-import Image from "next/image";
-import Button from "../Button/Button";
-import { useTranslation } from "react-i18next";
-import arrow from "/public/assets/images/ourProject/arrow.svg"
 
 const OurProjects = () => {
   const [activeSlide, setActiveSlide] = useState(0);
-  const {t} = useTranslation()
   const handleSlideChange = (index) => {
     setActiveSlide(index);
   };
@@ -64,50 +59,7 @@ const OurProjects = () => {
   const renderProjects = useMemo(
     () =>
       ourProjectArr.map((el, index) => (
-        <>
-          <div
-            className={scss.projects__item}
-            key={`${el.title}_${index}`}
-            id="project"
-          >
-            <div className={scss.item__info} id="project">
-              <div className={scss.item__title} id="project">
-                <h1>{t(el.title)}</h1>
-              </div>
-              <div className={scss.item__subtitle} id="project">
-                <p>{t(el.subtitle)}</p>
-              </div>
-              <div className={scss.item__links} id="project">
-                {el.links.map((link, i) => (
-                  <>
-                    <div
-                      key={`${link}_${i}`}
-                      className={scss.links__item}
-                      id="project"
-                    >
-                      <Image src={arrow} width={8} height={14} alt="arrow" />
-                      <p>{t(link)}</p>
-                    </div>
-                  </>
-                ))}
-              </div>
-              <Button
-                title={"button__reusable.site"}
-                variant={"btn__no_bg"}
-                withArrow={true}
-              />
-            </div>
-            <div className={scss.item__img} id="project">
-              <Image
-                src={el.imgSLider}
-                width={388}
-                height={230}
-                alt="sliderImg"
-              />
-            </div>
-          </div>
-          <ProjectsItem {...el} key={`${el.title}_${index}`} />
-        </>
+        <ProjectsItem {...el} key={`${el.title}_${index}`} />
       )),
     []
   );
