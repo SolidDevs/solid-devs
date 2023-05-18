649b5b09ced96c3e59f38f01c5918dc16a108a03
temangi <temirlan2.nuri@gmail.com>
2023-05-18 17:36:00 +0600

    fix

--- src/components/ourProjects/OurProjects.jsx
+++ src/components/ourProjects/OurProjects.jsx
@@ -6,9 +6,10 @@ import SampleNextArrow from "./arrows/nextArrow/SampleNextArrow";
 import SamplePrevArrow from "./arrows/prevArrow/SamplePrevArrow";
 import Paging from "./paging/Paging";
 import ProjectsItem from "./projectsItem/ProjectsItem";
+import Image from "next/image";
+import Button from "../Button/Button";
 
 const OurProjects = () => {
-
   const [activeSlide, setActiveSlide] = useState(0);
 
   const handleSlideChange = (index) => {
@@ -61,41 +62,58 @@ const OurProjects = () => {
   const renderProjects = useMemo(
     () =>
       ourProjectArr.map((el, index) => (
-        <div className={scss.projects__item} key={`${el.title}_${index}`} id="project">
-          <div className={scss.item__info}  id="project">
-            <div className={scss.item__title } id="project">
-              <h1>{t(el.title)}</h1>
+        <>
+          <div
+            className={scss.projects__item}
+            key={`${el.title}_${index}`}
+            id="project"
+          >
+            <div className={scss.item__info} id="project">
+              <div className={scss.item__title} id="project">
+                <h1>{t(el.title)}</h1>
+              </div>
+              <div className={scss.item__subtitle} id="project">
+                <p>{t(el.subtitle)}</p>
+              </div>
+              <div className={scss.item__links} id="project">
+                {el.links.map((link, i) => (
+                  <>
+                    <div
+                      key={`${link}_${i}`}
+                      className={scss.links__item}
+                      id="project"
+                    >
+                      <Image src={arrow} width={8} height={14} alt="arrow" />
+                      <p>{t(link)}</p>
+                    </div>
+                  </>
+                ))}
+              </div>
+              <Button
+                title={"button__reusable.site"}
+                variant={"btn__no_bg"}
+                withArrow={true}
+              />
             </div>
-            <div className={scss.item__subtitle}  id="project">
-              <p>{t(el.subtitle)}</p>
+            <div className={scss.item__img} id="project">
+              <Image
+                src={el.imgSLider}
+                width={388}
+                height={230}
+                alt="sliderImg"
+              />
             </div>
-            <div className={scss.item__links}  id="project">
-              {el.links.map((link, i) => (
-                <>
-                  <div key={`${link}_${i}`} className={scss.links__item}  id="project">
-                    <Image src={arrow} width={8} height={14} alt="arrow" />
-                    <p>{t(link)}</p>
-                  </div>
-                </>
-              ))}
-            </div>
-            <Button
-              title={"button__reusable.site"}
-              variant={"btn__no_bg"}
-              withArrow={true}
-            />
           </div>
-          <div className={scss.item__img}  id="project">
-            <Image src={el.imgSLider} width={388} height={230} alt="sliderImg" />
-          </div>
-        </div>
-        <ProjectsItem {...el} key={`${el.title}_${index}`} />
+          <ProjectsItem {...el} key={`${el.title}_${index}`} />
+        </>
       )),
     []
   );
   return (
     <div className={scss.projects} id="project">
-      <Slider {...sliderSettings} id="project">{renderProjects}</Slider>
+      <Slider {...sliderSettings} id="project">
+        {renderProjects}
+      </Slider>
     </div>
   );
 };
