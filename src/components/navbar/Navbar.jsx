96de362b799f503274fb6df4a084c2cd0aa91a00
luchok22 <aitbaev2021@mail.ru>
2023-05-24 04:08:40 +0600

    feat: adaptive technologies

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,10 +2,12 @@ import { useCallback, useEffect, useMemo, useState } from "react";
 import scss from "./Navbar.module.scss";
 import { useTranslation } from "react-i18next";
 import { useRouter } from "next/router";
-
-const Navbar = ({ navbarLinks, paramName }) => {
+import Image from "next/image";
+import arrow from "../../../public/images/technologies/arrow.svg";
+const Navbar = ({ navbarLinks, paramName, isOpen, handleOpen, children }) => {
   const router = useRouter();
   const [paramValue, setParamValue] = useState("");
+  const [activeButton, setActiveButton] = useState(null);
   useEffect(() => {
     const { query } = router;
     const value = query[paramName];
@@ -13,30 +15,63 @@ const Navbar = ({ navbarLinks, paramName }) => {
   }, [router, paramName]);
   const { t } = useTranslation("");
 
-  const handleClick = useCallback((item) => {
-    const query = router.query;
-    query[paramName] = t(item);
-    const path = {
-      pathname: router.pathname,
-      query,
-    };
-    router.push(path, path, { shallow: true });
-  }, [paramName, router])
-
+  const handleClick = useCallback(
+    (item) => {
+      const query = router.query;
+      query[paramName] = t(item);
+      const path = {
+        pathname: router.pathname,
+        query,
+      };
+      setActiveButton(item);
+      router.push(path, path, { shallow: true });
+    },
+    [paramName, router]
+  );
   const navbarItems = useMemo(
     () =>
       navbarLinks.map((item, index) => (
-        <button
-          key={`${item}_${index}`}
-          onClick={() => handleClick(item)}
-          className={
-            paramValue == item
-              ? scss.navbar__item_currentSelect
-              : scss.navbar__item_nonActive
-          }
-        >
-          {t(`navbar.${item}`)}
-        </button>
+        <div className={scss.navbar__links} key={`${item}_${index}`}>
+          <div className={scss.navbar__dekstop} key={`${item}_${index}`}>
+            <button
+              key={`${item}_${index}`}
+              onClick={() => handleClick(item)}
+              className={
+                paramValue == item
+                  ? scss.navbar__item_currentSelect
+                  : scss.navbar__item_nonActive
+              }
+            >
+              {t(`navbar.${item}`)}
+            </button>
+          </div>
+          <div className={scss.navbar__adaptive}>
+            <button
+              key={`${item}_${index}`}
+              onClick={() => {
+                handleClick(item);
+                handleOpen();
+              }}
+              className={
+                paramValue == item
+                  ? scss.navbar__item_currentSelect
+                  : scss.navbar__item_nonActive
+              }
+            >
+              {t(`navbar.${item}`)}
+              <div
+                className={
+                  handleOpen ? scss.title__right_active : scss.title__right
+                }
+              >
+                <Image src={arrow} width={14} height={8} />
+              </div>
+            </button>
+            {activeButton === item && (
+              <div className={scss.navbar__technologies}>{children}</div>
+            )}
+          </div>
+        </div>
       )),
     [navbarLinks, paramValue, handleClick, t]
   );
