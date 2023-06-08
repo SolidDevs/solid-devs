288a425a1ba3133efc530f7a2d69154b8a34de4d
Almo <temirkanovalmanbet5@gmail.com>
2023-06-08 18:33:35 +0300

    fix: Block-Navigation

--- src/components/carierInfoBlock/CarierInfoBlock.jsx
+++ src/components/carierInfoBlock/CarierInfoBlock.jsx
@@ -1,58 +1,67 @@
 import { useTranslation } from "react-i18next";
 import Button from "../Button/Button";
-import scss from "./CarierInfoBlock.module.scss"
+import scss from "./CarierInfoBlock.module.scss";
 import { useMemo } from "react";
 import { teamStepsList, practicumStepsList } from "@/constants/carier";
+import Navigation from "../Navigation/Navigation";
 
 const CarierInfoBlock = ({ variant }) => {
   const { t } = useTranslation();
-  const title = variant == "practicum" ? t("carier.practicumtitle") : t("carier.teamTitle")
-  const description = variant == "practicum" ? t("carier.practicumDescription") : t("carier.teamDescription")
-  const data = variant == "practicum" ? practicumStepsList : teamStepsList
+  const title =
+    variant == "practicum" ? t("carier.practicumtitle") : t("carier.teamTitle");
+  const description =
+    variant == "practicum"
+      ? t("carier.practicumDescription")
+      : t("carier.teamDescription");
+  const data = variant == "practicum" ? practicumStepsList : teamStepsList;
 
   const showLine = useMemo(() => {
     if (variant == "team") {
-      return (
-        <div className={scss.carier__line}></div>
-      )
+      return <div className={scss.carier__line}></div>;
     }
-  }, [variant])
+  }, [variant]);
 
-  const renderCards = useMemo(() => (
-    data.map((el, index) => (
-      <div className={scss.step} key={`${el.title}_${el.description}`}>
-        <div className={scss.step__numeration}>{index + 1}</div>
-        <div className={scss.step__text__container}>
-          <div className={scss.text__title}>{t(el.title)}</div>
-          <div className={scss.text__description}>{t(el.description)}</div>
+  const renderCards = useMemo(
+    () =>
+      data.map((el, index) => (
+        <div className={scss.step} key={`${el.title}_${el.description}`}>
+          <div className={scss.step__numeration}>{index + 1}</div>
+          <div className={scss.step__text__container}>
+            <div className={scss.text__title}>{t(el.title)}</div>
+            <div className={scss.text__description}>{t(el.description)}</div>
+          </div>
         </div>
-      </div>
-    ))
-  ), [data, t])
+      )),
+    [data, t]
+  );
 
   const showText = useMemo(() => {
     if (variant == "team") {
       return (
         <p className={scss.content__letter__text}>{t("carier.writeLetter")}</p>
-      )
+      );
     }
-  }, [variant, t]
-  )
+  }, [variant, t]);
 
   return (
-    <div className={scss.carier__wrapper}>
-      <div className={scss.text__content}>
-        <p className={scss.content__title}>{title}</p>
-        <p className={scss.content__description}>{description}</p>
-        {showText}
-        <Button variant="btn__light_blue" withArrow={true} title={t("carier.btnText")} />
+    <>
+      <Navigation title={t("navigation.Career_description")} />
+      <div className={scss.carier__wrapper}>
+        <div className={scss.text__content}>
+          <p className={scss.content__title}>{title}</p>
+          <p className={scss.content__description}>{description}</p>
+          {showText}
+          <Button
+            variant="btn__light_blue"
+            withArrow={true}
+            title={t("carier.btnText")}
+          />
+        </div>
+        <div className={scss.steps__container}>{renderCards}</div>
+        {showLine}
       </div>
-      <div className={scss.steps__container}>
-        {renderCards}
-      </div>
-      {showLine}
-    </div>
-  )
+    </>
+  );
 };
 
 export default CarierInfoBlock;
