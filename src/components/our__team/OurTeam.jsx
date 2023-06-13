c106d6e70454ffd8c9e4b01aa0aaf1f5a1cd7469
Improove3567 <jyldyzbekov06aiti@gmail.com>
2023-06-13 14:00:18 +0600

    fix:images

--- src/components/our__team/OurTeam.jsx
+++ src/components/our__team/OurTeam.jsx
@@ -1,21 +1,27 @@
 import { useTranslation } from "react-i18next";
 import SectionContainer from "../layoutComponent/SectionContainer";
 import scss from "./OurTeam.module.scss";
-import { useMemo } from "react";
-import { ourTeamData } from "@/constants/ourTeam";
+import { useEffect, useMemo } from "react";
 import OurTeamCard from "./our__team_card/OurTeamCard";
+import useTeam from "@/hooks/useTeam";
 
 const OurTeam = () => {
   const { t } = useTranslation("");
+  const { team, getTeam } = useTeam();
+
+  useEffect(() => {
+    getTeam();
+  }, [])
+
   const ourTeamItems = useMemo(
     () =>
-      ourTeamData.map((item, index) => (
+      team?.map((item, index) => (
         <OurTeamCard key={`${item.name}_${index}`} {...item} />
       )),
-    []
+    [team]
   );
   return (
-    <SectionContainer title={t("ourTeam.title")}id="team" >
+    <SectionContainer title={t("ourTeam.title")} id="team" >
       <div className={scss.ourTeam} id="team">{ourTeamItems}</div>
     </SectionContainer>
   );
