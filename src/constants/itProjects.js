500c47f7c1b6f69e4452d7fd29c808c9a4fae124
Improove3567 <jyldyzbekov06aiti@gmail.com>
2023-06-14 02:18:48 +0600

    fix:images

--- src/constants/itProjects.js
+++ src/constants/itProjects.js
@@ -1,7 +1,15 @@
 import Crypto from "../../public/assets/images/itProjects/crypto.svg";
-import ElTrade from "../../public/assets/images/itProjects/ElecTrade.svg";
-import Server from "../../public/assets/images/itProjects/Serverless.svg";
-import Transport from "../../public/assets/images/itProjects/Transport.svg";
+import ElTrade from "../../public/assets/images/itProjects/Frame 431.svg";
+import Server from "../../public/assets/images/itProjects/Frame 432.svg";
+import Transport from "../../public/assets/images/itProjects/Frame 433.svg";
+import Transformation from "../../public/assets/images/itProjects/Frame 434.svg";
+import Technologies from "../../public/assets/images/itProjects/Frame 435.svg";
+import AddTech from "../../public/assets/images/itProjects/Frame 436.svg";
+import Training from "../../public/assets/images/itProjects/Frame 437.svg";
+import Streaming from "../../public/assets/images/itProjects/Frame 438.svg";
+import Health from "../../public/assets/images/itProjects/Frame 439.svg";
+import Tech from "../../public/assets/images/itProjects/Frame 440.svg";
+import Things from "../../public/assets/images/itProjects/Frame 441.svg";
 
 export const itProjects = [
   {
@@ -21,35 +29,35 @@ export const itProjects = [
     title: "itProjects.title__transport",
   },
   {
-    icon: Server,
+    icon: Transformation,
     title: "itProjects.title__digital",
   },
   {
-    icon: Server,
+    icon: Technologies,
     title: "itProjects.title__technologies",
   },
   {
-    icon: Server,
+    icon: AddTech,
     title: "itProjects.title__adTech",
   },
   {
-    icon: Server,
+    icon: Training,
     title: "itProjects.title__training",
   },
   {
-    icon: Server,
+    icon: Streaming,
     title: "itProjects.title__streaming",
   },
   {
-    icon: Server,
+    icon: Health,
     title: "itProjects.title__healthTech",
   },
   {
-    icon: Server,
+    icon: Tech,
     title: "itProjects.title__adTech2",
   },
   {
-    icon: Server,
+    icon: Things,
     title: "itProjects.title__things",
   },
 ];
